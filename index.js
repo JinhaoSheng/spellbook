ec422cf2a2097bc0f18ce64b53b8e55eee353cf5
Jinhao Sheng <shengj1@rose-hulman.edu>
2018-06-04 19:02:10 -0400

    third and fourth stages

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#button')
 
 const sayHi = function(){
     const heading = document.querySelector('#heading2')
     heading.textContent = 'I changed it'
 }
 
-
+const form = document.querySelector('form')
 button.addEventListener('click', sayHi)
+function myFunction(){
+    const heading2 = document.querySelector("#heading3")
+    heading2.textContent = form.elements["formtext"].value
+}
+
+
