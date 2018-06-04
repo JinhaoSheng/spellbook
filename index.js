894a555b2aca58ef6a5c6b65dd98ff155207b038
Jinhao Sheng <shengj1@rose-hulman.edu>
2018-06-04 16:19:49 -0400

    second stage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('button')
 
 const sayHi = function(){
-    const heading = document.querySelector('h1')
+    const heading = document.querySelector('#heading2')
     heading.textContent = 'I changed it'
 }
 
