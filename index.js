3a5eb8d633c5c9919aed108981e1868f263c5170
Jinhao Sheng <shengj1@rose-hulman.edu>
2018-06-05 16:10:18 -0400

    Finish all requirements

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const button = document.querySelector('#button')
-
-const sayHi = function(){
-    document
-        .querySelector("#heading2")
-        .textContent = 'I changed it!'
-}
-button.addEventListener('click', sayHi)
-
 const form = document.querySelector('form')
-function myFunction(){
-    const heading2 = document.querySelector("div")
-    heading2.textContent += form.elements["formtext"].value + ' '
+
+
+
+const changeHeading = function(ev) {
+
+  ev.preventDefault()
+  const f = ev.target
+  
+  const enteredSpell = document.createTextNode(f.spellName.value)
+  const enteredSpell2 = document.createTextNode(f.spellName2.value)
+  const spellUl = document.querySelector("#spells")
+  let a = createListUsingSpan(enteredSpell, enteredSpell2)
+  spellUl.appendChild(a)
+}
+
+function createListUsingSpan(enteredSpell, enteredSpell2){
+    const span = document.createElement("span")
+    const span2 = document.createElement("span")
+    span.appendChild(enteredSpell)
+    span.appendChild(enteredSpell2)
+    return createList(span, span2)
+}
+
+function createList(span, span2){
+    const list = document.createElement("li")
+    list.appendChild(span)
+    list.appendChild(span2)
+    return list
 }
 
 
+
+
+form.addEventListener('submit', changeHeading)
